d960fc9adb246b2c6aead72059fe9e314869f035
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-09-22 12:26:45 +0530

    ch69

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/users");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const ErrorHandler = require("../utils/errorHandler");
+const sendToken = require("../utils/jwtToken")
 
 //Get current user profile => /api/v1/me 
 
@@ -12,4 +13,28 @@ exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
         success: true,
         data: user
     })
+})
+
+
+// Update current user Password. => api/v1/password/change
+
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+
+    const user = await User.findById(req.user.id).select("+password");
+    
+    //check previous user password. =>
+
+    const isMatched = await user.comparePassword(req.body.currentPassword);
+
+    if(!isMatched) {
+        return next(new ErrorHandler("Old Password is incorrect", 401))
+    }
+
+    user.password = req.body.newPassword;
+
+    await user.save();
+
+    sendToken(user, 200, res);
+
+
 })
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express= require('express');
 const router= express.Router();
 
-const {getUserProfile} = require("../controllers/userController")
+const {getUserProfile, updatePassword} = require("../controllers/userController")
 
 const {isAuthenticated} = require("../middlewares/auth");
 
 router.route("/me").get(isAuthenticated, getUserProfile);   // if authenticated will not be written it will throw error, can not read the property of undefined becz in authentication only we are attaching userid. 
 
+router.route("/password/update").put(isAuthenticated, updatePassword)
 module.exports = router;
